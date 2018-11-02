6dc7bbdde29ea2a229eca260957dcaa387438b79
William Dale <williambdale@gmail.com>
2018-11-02 17:43:54 -0400

    Add functionality to age

--- indecision_app/src/app.js
+++ indecision_app/src/app.js
@@ -30,8 +30,8 @@ function getLocation(location) {
 
 let templateTwo = (
   <div>
-    <h1>{user.name}</h1>
-    <p>Age: {user.age}</p>
+    <h1>{user.name ? user.name : "Anonymous"}</h1>
+    {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
     {getLocation(user.location)}
   </div>
 )
