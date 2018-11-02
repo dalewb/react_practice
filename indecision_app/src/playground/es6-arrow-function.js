4a02f63c775139af0ba3c9841f6592cd46834c7a
William Dale <williambdale@gmail.com>
2018-11-02 18:26:02 -0400

    Add arrow function practice

--- indecision_app/src/playground/es6-arrow-function.js
+++ indecision_app/src/playground/es6-arrow-function.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // const square = function (x) {
 //   return x * x
 // }
@@ -13,14 +11,12 @@
 // console.log(squareArrow(100))
 
 
-var fullName = 'Julia Roberts';
+const fullName = 'Julia Roberts'
 
 // let getFirstName = (name) => {
 //   return name.split(' ')[0]
 // }
 
-var getFirstName = function getFirstName(name) {
-  return name.split(' ')[0];
-};
+let getFirstName = (name) => name.split(' ')[0]
 
 console.log(getFirstName(fullName));
