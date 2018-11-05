5b01cb01b3b44016b8f5feee6efcbe8f9703d839
William Dale <williambdale@gmail.com>
2018-11-05 11:28:17 -0500

    Add student class

--- indecision_app/src/playground/es6-classes-1.js
+++ indecision_app/src/playground/es6-classes-1.js
@@ -3,18 +3,28 @@ class Person {
     this.name = name
     this.age = age
   }
-
   getGreeting() {
     return `Hello, my name is ${this.name}!`
   }
-
   getDescription() {
     return `${this.name} is ${this.age} years old.`
+  }
+}
+
+class Student extends Person {
+  constructor(name = "Anon", age = 0, major = "Undecided") {
+    this.name = name
+    this.age = age
+    this.major = major
+  }
+
+  getDescription() {
+    return `${this.name} is ${this.age} years old. I am studying ${this.major}`
   }
 }
 
 const me = new Person("Billy Dale", 31)
 console.log(me.getDescription());
 
-const other = new Person()
-console.log(other.getDescription());
+const student = new Student("John Smith", 19)
+console.log(student.getDescription());
