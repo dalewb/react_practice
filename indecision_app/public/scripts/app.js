317bb1fc8ccc0bf3ba0dc29e5982ca67e250d4e0
William Dale <williambdale@gmail.com>
2018-11-06 17:15:09 -0500

    Remove old counter code

--- indecision_app/public/scripts/app.js
+++ indecision_app/public/scripts/app.js
@@ -87,35 +87,3 @@ var Counter = function (_React$Component) {
 }(React.Component);
 
 ReactDOM.render(React.createElement(Counter, null), document.getElementById('app'));
-
-// let count = 0
-//
-// const plusOne = () => {
-//   count++
-//   renderCounterApp()
-// }
-//
-// const minusOne = () => {
-//   count > 0 ? count-- : null
-//   renderCounterApp()
-// }
-//
-// const reset = () => {
-//   count = 0
-//   renderCounterApp()
-// }
-//
-// const renderCounterApp = () => {
-//   const templateTwo = (
-//     <div>
-//       <h1>Count: {count}</h1>
-//       <button className="button" onClick={plusOne}>+1</button>
-//       <button className="button" onClick={minusOne}>-1</button>
-//       <button className="button" onClick={reset}>Reset</button>
-//     </div>
-//   )
-//
-//   ReactDOM.render(templateTwo, appRoot)
-// }
-//
-// renderCounterApp()
