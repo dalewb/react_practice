4c6b02018075fd7361ede6110d87452091524cc0
William Dale <williambdale@gmail.com>
2018-11-06 17:40:36 -0500

    Change IndecisionApp component to have state

--- indecision_app/src/app.js
+++ indecision_app/src/app.js
@@ -1,14 +1,19 @@
 class IndecisionApp extends React.Component {
-  render() {
-    const title = "Indecision"
-    const subtitle = "Put your life in the hands of a computer"
-    const options = ['thing one', 'thing two', 'thing four']
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: "Indecision",
+      subtitle: "Put your life in the hands of a computer",
+      options: ['thing one', 'thing two', 'thing three'],
+    }
+  }
 
+  render() {
     return (
       <div>
-        <Header title={title} subtitle={subtitle}/>
-        <Action />
-        <Options options={options}/>
+        <Header title={this.state.title} subtitle={this.state.subtitle}/>
+        <Action hasOptions={this.state.options.length > 0} />
+        <Options options={this.state.options}/>
         <AddOption />
       </div>
     )
@@ -34,7 +39,12 @@ class Action extends React.Component {
   render() {
     return (
       <div>
-        <button onClick={this.handlePick}>What should I do?</button>
+        <button
+          onClick={this.handlePick}
+          disabled={!this.props.hasOptions}
+        >
+          What should I do?
+        </button>
       </div>
     )
   }
