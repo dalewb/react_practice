ddfde489b96a082e8c21bb7fe0d0b3e7c3d7791a
William Dale <williambdale@gmail.com>
2018-11-09 22:58:57 -0500

    Add webpack dev server

--- indecision_app/src/components/AddOption.js
+++ indecision_app/src/components/AddOption.js
@@ -13,6 +13,7 @@ export default class AddOption extends React.Component {
     e.preventDefault()
     const option = e.target.elements.option.value.trim()
     const error = this.props.handleAddOption(option)
+    console.log(error);
     if (error) {
       this.setState(() => ({ error }))
     } else {
