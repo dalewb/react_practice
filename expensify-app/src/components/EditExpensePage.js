41ee2b727ce4eae49a5d5aa1f902cceb64decf1f
William Dale <williambdale@gmail.com>
2018-11-11 14:06:55 -0500

    Break out components into seperate files'

---
+++ expensify-app/src/components/EditExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EditExpensePage = () => (
+  <div>
+    This is my EditExpense component
+  </div>
+)
+
+export default EditExpensePage;
--- expensify-app/src/routers/AppRouter.js
+++ expensify-app/src/routers/AppRouter.js
@@ -1,46 +1,12 @@
 import React from 'react';
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
-
-const ExpenseDashboardPage = () => (
-  <div>
-    This is my dashboard component
-  </div>
-)
-
-const AddExpensePage = () => (
-  <div>
-    This is my AddExpense component
-  </div>
-)
-
-const EditExpensePage = () => (
-  <div>
-    This is my EditExpense component
-  </div>
-)
-
-const HelpPage = () => (
-  <div>
-    This is my Help component
-  </div>
-)
-
-const NotFoundPage = () => (
-  <div>
-    404 - <Link to="/">Go Home</Link>
-  </div>
-)
-
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-    <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
-    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-  </header>
-)
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
+import AddExpensePage from '../components/AddExpensePage';
+import EditExpensePage from '../components/EditExpensePage';
+import HelpPage from '../components/HelpPage';
+import NotFoundPage from '../components/NotFoundPage';
+import Header from '../components/Header';
 
 const AppRouter = () => (
   <BrowserRouter>
@@ -55,6 +21,6 @@ const AppRouter = () => (
       </Switch>
     </div>
   </BrowserRouter>
-)
+);
 
-export default AppRouter
+export default AppRouter;
