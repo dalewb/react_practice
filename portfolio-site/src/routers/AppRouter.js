9dc52ccea8f82716854bc34d4cfcb08aae3e3af0
William Dale <williambdale@gmail.com>
2018-11-11 19:56:50 -0500

    Add HomePage to portfolio app

--- portfolio-site/src/routers/AppRouter.js
+++ portfolio-site/src/routers/AppRouter.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import HomePage from '../components/HomePage';
 import NotFoundPage from '../components/NotFoundPage';
 import Header from '../components/Header';
 
@@ -9,6 +10,7 @@ const AppRouter = () => (
     <div>
       <Header />
       <Switch>
+        <Route path="/" component={HomePage} exact={true}/>
         <Route component={NotFoundPage} />
       </Switch>
     </div>
