f35e9d3f915f471923c9871ae59f4a786bfd2b85
William Dale <williambdale@gmail.com>
2018-11-11 20:55:24 -0500

    Change ContactMePage to ContactPage

--- portfolio-site/src/routers/AppRouter.js
+++ portfolio-site/src/routers/AppRouter.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import HomePage from '../components/HomePage';
 import PortfolioPage from '../components/PortfolioPage';
 import ItemPage from '../components/ItemPage';
-import ContactMePage from '../components/ContactMePage'
+import ContactPage from '../components/ContactPage'
 import NotFoundPage from '../components/NotFoundPage';
 import Header from '../components/Header';
 
@@ -16,7 +16,7 @@ const AppRouter = () => (
         <Route path="/" component={HomePage} exact={true}/>
         <Route path="/portfolio" component={PortfolioPage} exact={true}/>
         <Route path="/portfolio/:id" component={ItemPage}/>
-        <Route path="/contact" component={ContactMePage}/>
+        <Route path="/contact" component={ContactPage}/>
         <Route component={NotFoundPage} />
       </Switch>
     </div>
