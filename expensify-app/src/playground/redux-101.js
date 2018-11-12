861d53f414b7d2f6bf75af5228c6297fd58da496
William Dale <williambdale@gmail.com>
2018-11-12 08:56:03 -0500

    Add sstore.subscribe and action payloads

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -3,12 +3,14 @@ import { createStore } from 'redux';
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
+      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + 1
+        count: state.count + incrementBy
       };
     case 'DECREMENT':
+      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - 1
+        count: state.count - decrementBy
       }
     case 'RESET':
       return {
@@ -19,23 +21,25 @@ const store = createStore((state = { count: 0 }, action) => {
   }
 });
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+})
 
 // Actions - then an object gets sent to a createStore
 
 // Increment the count
 store.dispatch({
-  type: 'INCREMENT'
+  type: 'INCREMENT',
+  incrementBy: 5,
 });
 
 // Decrement the count
 store.dispatch({
-  type: 'DECREMENT'
+  type: 'DECREMENT',
+  decrementBy: 10,
 });
 
 // Reset the count
 store.dispatch({
   type: 'RESET'
 });
-
-console.log(store.getState());
