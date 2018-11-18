4a32e48845c5f909edad5f4b994da4e318ad12f1
William Dale <williambdale@gmail.com>
2018-11-18 16:47:08 -0500

    Add payload for action dispatch in redux-101

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,11 +1,17 @@
 import { createStore } from 'redux';
 
+// Action Generators - functions that return action objects
+
+const incrementCount = (payload = {}) => ({
+  type: 'INCREMENT',
+  incrementBy: typeof payload.incrementBy === 'number' ? payload.incrementBy : 1
+})
+
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + incrementBy
+        count: state.count + action.incrementBy
       };
     case 'DECREMENT':
       const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
@@ -33,10 +39,9 @@ const unsubscribe = store.subscribe(() => {
 // Actions - then an object gets sent to a createStore
 
 // Increment the count
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 5,
-});
+store.dispatch(incrementCount({ incrementBy: 5 }))
+
+store.dispatch(incrementCount())
 
 // Decrement the count
 store.dispatch({
